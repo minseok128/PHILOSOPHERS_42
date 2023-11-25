0568583aa1199e71ce5e97dfeea633c6673b2017
michang <minseok128128@gmail.com>
2023-11-25 16:26:30 +0900

    initialize: header

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 16:20:19 by michang           #+#    #+#             */
+/*   Updated: 2023/11/25 16:20:20 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
