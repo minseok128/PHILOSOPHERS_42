83a31ab2b300151cd60653edf820c3f91e2d9079
michang <minseok128128@gmail.com>
2023-11-25 17:30:41 +0900

    implement: t_info

--- philo/philo.c
+++ philo/philo.c
@@ -12,9 +12,21 @@
 
 #include "philo.h"
 
+void	init_info(char **argv, t_info *info)
+{
+	info->n_of_philos = ft_atol(argv[1]);
+	info->t_to_die = ft_atol(argv[2]);
+	info->t_to_sleep = ft_atol(argv[3]);
+	info->n_of_t_must_eat = ft_atol(argv[4]);
+}
+
 int	main(int argc, char **argv)
 {
-	(void) argv;
-	argc = 1;
+	t_info	info;
+
+	if (!(argc == 4 || argc == 5))
+		return (1);
+	init_info(argv, &info);
+	write(0, "hello philo!", 12);
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -19,4 +19,13 @@
 # include <stdlib.h>
 # include <stdio.h>
 
+typedef struct s_info {
+    long long   n_of_philos;
+    long long   t_to_die;
+    long long   t_to_sleep;
+    long long   n_of_t_must_eat;
+} t_info;
+
+long long	ft_atol(const char *str);
+
 #endif
